68dbed7db7d00d68cf72d2068c2189cd000face9
Amiraslanov-1 <99805882+Amiraslanov-1@users.noreply.github.com>
2022-03-11 04:33:21 +0400

    Task 1-2

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+     internal class Product
+    {
+        public string NameProd;
+        public string ProdBrand;
+        public int Price;
+    }
+}
